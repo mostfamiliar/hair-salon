713b1c5924bcab116d7130638cb0304b408afcbd
Torrence Stratton <tlstratton@gmail.com>
2016-02-26 08:16:11 -0800

    add initial project setup

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=hair_salon.db';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password)
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
+    ));
+
+
+
+    return $app;
+ ?>
---
+++ tests/StylistTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+
+require_once "src/Stylist.php";
+
+$server = 'mysql:host=localhost;dbname=hair_salon_test.db';
+$username = 'root';
+$password = 'root';
+$DB = new PDO($server, $username, $password);
+
+
+
+class  blblkbjlbkjb  extends PHPUnit_Framework_TestCase{
+
+
+}
+ ?>
