c300a91542efed313985f2c3036cf7e4c9c71259
Torrence Stratton <tlstratton@gmail.com>
2016-02-26 08:41:22 -0800

    add 4 passing specs

--- src/Stylist.php
+++ src/Stylist.php
@@ -1,5 +1,57 @@
 <?php
-class  {
+    class Stylist {
+            private $name;
+            private $id;
 
-    
+            function __construct($name, $id = null)
+            {
+                $this->name = $name;
+                $this->id = $id;
+            }
+
+            function setName($new_name)
+            {
+                $this->name = $new_name;
+            }
+
+            function getName()
+            {
+                return $this->name;
+            }
+
+            function getId()
+            {
+                return $this->id;
+            }
+
+            function save()
+            {
+                $GLOBALS['DB']->exec("INSERT INTO stylist (name) VALUES ('{$this->getName()}');");
+                $this->id = $GLOBALS['DB']->lastInsertId();
+            }
+
+            static function getAll()
+            {
+                $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylist;");
+                var_dump($returned_stylists);
+                $stylists = array();
+
+                foreach($returned_stylists as $stylist)
+                {
+                    $name = $stylist['name'];
+                    $id = $stylist['id'];
+                    $new_stylist = new Stylist($name, $id);
+                    array_push($stylists, $new_stylist);
+                }
+
+                return $stylists;
+            }
+
+            static function deleteAll()
+            {
+                $GLOBALS['DB']->exec("DELETE FROM stylist");
+            }
+
+
+
 } ?>
--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -7,14 +7,81 @@
 
 require_once "src/Stylist.php";
 
-$server = 'mysql:host=localhost;dbname=hair_salon_test.db';
+$server = 'mysql:host=localhost;dbname=hair_salon_test';
 $username = 'root';
 $password = 'root';
 $DB = new PDO($server, $username, $password);
 
 
 
-class  blblkbjlbkjb  extends PHPUnit_Framework_TestCase{
+class  StylistTest  extends PHPUnit_Framework_TestCase{
+
+
+        protected function tearDown()
+        {
+          Stylist::deleteAll();
+        }
+
+        function test_getName()
+        {
+            //Arrange
+            $name = "Bob";
+            $new_stylist = new Stylist($name);
+
+            //Act
+            $result = $new_stylist->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $name = "Trish";
+            $id = 1;
+            $new_stylist = new Stylist($name, $id);
+
+            //Act
+            $result = $new_stylist->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $name = "Sean";
+            $new_stylist = new Stylist($name);
+            $new_stylist->save();
+
+            //Act
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals($new_stylist, $result[0]);
+
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Sean";
+            $new_stylist = new Stylist($name);
+            $new_stylist->save();
+
+            $name2 = "Patty";
+            $new_stylist2 = new Stylist($name2);
+            $new_stylist2->save();
+
+            //Act
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals([$new_stylist, $new_stylist2], $result);
+        }
+
 
 
 }
