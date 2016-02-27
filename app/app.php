e1ce7510ff5e119a5a7c23740340095e13ed596c
Torrence Stratton <tlstratton@gmail.com>
2016-02-26 16:11:19 -0800

    add form validation

--- app/app.php
+++ app/app.php
@@ -25,7 +25,11 @@
 
     $app->post("/stylists", function() use ($app)
     {
-        $stylist = new Stylist($_POST['stylist']);
+        $name = $_POST['stylist'];
+        $name_no_punc = preg_replace('/[^a-z0-9]+/i', " ", $name);
+        $name_lower = strtolower($name_no_punc);
+        $name_input = ucfirst($name_lower);
+        $stylist = new Stylist($name_input);
         $stylist->save();
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
 
@@ -35,7 +39,10 @@
     {
         $name = $_POST['client_name'];
         $stylist_id = $_POST['stylist_id'];
-        $new_client = new Client($name, $stylist_id, $id = null);
+        $name_no_punc = preg_replace('/[^a-z0-9]+/i', " ", $name);
+        $name_lower = strtolower($name_no_punc);
+        $name_input = ucfirst($name_lower);
+        $new_client = new Client($name_input, $stylist_id, $id = null);
         $new_client->save();
         $stylist = Stylist::find($stylist_id);
         return $app['twig']->render('stylists.html.twig', array('stylist'=> $stylist, 'clients' => $stylist->getClients()));
@@ -57,16 +64,22 @@
     $app->patch("/stylist/{id}", function ($id) use ($app)
     {
         $new_name = $_POST['new_name'];
+        $name_no_punc = preg_replace('/[^a-z0-9]+/i', " ", $name);
+        $name_lower = strtolower($name_no_punc);
+        $name_input = ucfirst($name_lower);
         $stylist = Stylist::find($id);
-        $stylist->update($new_name);
+        $stylist->update($name_input);
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
     $app->patch("/clients/{id}", function ($id) use ($app)
     {
         $new_name = $_POST['new_name'];
+        $name_no_punc = preg_replace('/[^a-z0-9]+/i', " ", $name);
+        $name_lower = strtolower($name_no_punc);
+        $name_input = ucfirst($name_lower);
         $client = Client::find($id);
-        $client->updateName($new_name);
+        $client->updateName($name_input);
         $stylist_id = $client->getStylistId();
         $stylist = Stylist::find($stylist_id);
         return $app['twig']->render('stylists.html.twig', array('stylist'=> $stylist, 'clients' => $stylist->getClients()));
@@ -75,7 +88,6 @@
     $app->delete("/stylist/{id}", function ($id) use ($app)
     {
         $stylist= Stylist::find($id);
-        var_dump($stylist);
         $stylist->delete();
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
